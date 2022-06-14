4e1c671d6490d44b4745eb86ccea8eee2b6042b7
stephen <stevedogbey@gmail.com>
2022-06-14 06:11:37 +0000

    arithmetic lab completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
+
+let random = Math.floor(Math.random() * 10);
+
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4
+
+let max = Math.max(2, 4, 6, 8, 10, 20);
 // Write your code here
